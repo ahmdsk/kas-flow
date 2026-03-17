fc4496875d918e2a8216bb56222769324f89e5fc
Ahmad Shaleh <ahmadsaleh2409@gmail.com>
2026-03-17 15:48:41 +0700

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: Scaffold(
+        body: const Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
+}
